c8191744b4b03491f0beaf2d1ee129aa5de279b5
Rimmesh <rimmeshete@example.com>
2024-03-01 14:58:12 -0800

    commit

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,57 +1,31 @@
 #include "binary_trees.h"
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- * Return: If no common ancestors return NULL, else return common ancestor
+ * binary_trees_ancestor - function that checks an ancestor
+ * @first: First node
+ * @second: Second node
+ * Return: the node of the ancestor
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
-binary_tree_t *mom, *pop;
-if (!first || !second)
-return (NULL);
-if (first == second)
-return ((binary_tree_t *)first);
-mom = first->parent, pop = second->parent;
-if (first == pop || !mom || (!mom->parent && pop))
-return (binary_trees_ancestor(first, pop));
-else if (mom == second || !pop || (!pop->parent && mom))
-return (binary_trees_ancestor(mom, second));
-return (binary_trees_ancestor(mom, pop));
-
-size_t first_depth, second_depth;
+binary_tree_t *p, *q;
 if (first == NULL || second == NULL)
-return (NULL);
-if (first == second)
-return ((binary_tree_t *)first);
-if (first->parent == second->parent)
-return ((binary_tree_t *)first->parent);
-if (first == second->parent)
-return ((binary_tree_t *)first);
-if (first->parent == second)
-return ((binary_tree_t *)second);
-for (first_depth = depth(first); first_depth > 1; first_depth--)
-first = first->parent;
-for (second_depth = depth(second); second_depth > 1; second_depth--)
-second = second->parent;
-if (first == second)
-return ((binary_tree_t *)first);
-if (first->parent == second->parent)
-return ((binary_tree_t *)first->parent);
-if (first == second->parent)
-return ((binary_tree_t *)first);
-if (first->parent == second)
-return ((binary_tree_t *)second);
-else
+{
 return (NULL);
 }
-/**
- * depth - Measures the depth of a node in a binary tree
- * @tree: pointer to node to measure the depth
- * Return: If tree is NULL, your function must return 0, else return the depth
- */
-size_t depth(const binary_tree_t *tree)
+if (first == second)
 {
-return ((tree->parent != NULL) ? 1 + depth(tree->parent) : 0);
+return ((binary_tree_t *)first);
+}
+p = first->parent;
+q = second->parent;
+if (p == NULL || first == q || (!p->parent && q))
+{
+return (binary_trees_ancestor(first, q));
+}
+else if (q == NULL || p == second || (!q->parent && p))
+{
+return (binary_trees_ancestor(p, second));
+}
+return (binary_trees_ancestor(p, q));
 }
